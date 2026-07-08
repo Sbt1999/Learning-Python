c9e3379e2b00b5e34d4804271e560437eabb83a1
Shobhit <shobhitkatiyar56@gmail.com>
2026-07-08 10:53:39 +0530

    feat: written api to upload the pdf and store it in local Storage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi import UploadFile, File
+from pathlib import Path
+import shutil
 # It creates one FastApi() application
 app = FastAPI()
 
@@ -7,3 +9,25 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "AI Interview Backend is running"}
+
+@app.post("/upload")
+def upload_resume(file: UploadFile = File(...)):
+    return {
+        "filename": file.filename,
+        "content_type" : file.content_type
+    }
+
+# In the next step I will create upload api so that it will save the pdf in uploads folder
+UPLOAD_DIR = Path("uploads")
+UPLOAD_DIR.mkdir(exist_ok=True)
+
+@app.post("/fileUpload")
+def upload_resum(file: UploadFile = File(...)):
+    destination = UPLOAD_DIR / file.filename
+
+    with open(destination, "wb") as buffer:
+        shutil.copyfileobj(file.file,buffer)
+    return {
+        "message": "Resume uploaded successfuly",
+        "filename": file.filename
+    }
